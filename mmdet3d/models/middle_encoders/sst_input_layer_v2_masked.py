fix bugs with masking

mmdet3d/models/middle_encoders/sst_input_layer_v2_masked.py
@@ -118,8 +118,8 @@ class SSTInputLayerV2Masked(SSTInputLayerV2):
         gt_dict["points_per_voxel_padding"] = gt_points_padding
 
         # Masking voxels: True -> masked, False -> unmasked
-        mask = torch.rand(len(voxel_feats), device=device) < self.masking_ratio
-        masked_idx, unmasked_idx = mask.nonzero(), (~mask).nonzero()
+        mask = torch.rand(len(voxel_feats), device=voxel_feats.device) < self.masking_ratio
+        masked_idx, unmasked_idx = mask.nonzero().ravel(), (~mask).nonzero().ravel()
         n_masked_voxels, n_unmasked_voxels = len(masked_idx), len(unmasked_idx)
 
         # Get info for decoder input, Might drop voxels